f0fe951d1f262f97a7e3b2b7d7a5a9ee4cffe2eb
ArisoN <arison@duck.com>
2026-08-30 00:54:41 +0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
       packages = forAllSystems ({ pkgs }: {
         default = pkgs.rustPlatform.buildRustPackage {
           pname = "flatpak-helper";
-          version = "1.0.0";
+          version = "1.0.1";
           
           src = ./.;
           
